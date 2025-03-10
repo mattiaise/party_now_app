51f0f531f4c8f4ba4c5bf412cbd08ac6ea4f65be
mattia.isetta <mattia.isetta@studenti.unimi.it>
2025-03-10 12:59:38 +0100

    implemented bottom navbar and tailwind in layout

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BottomNavBar from "../components/BottomNavBar"; 
+
+const App = () => {
+  return (
+    <div>
+      <h1>Benvenuto nella mia App</h1>
+      {/* Contenuto della tua applicazione */}
+      <BottomNavBar />
+    </div>
+  );
+};
+
+export default App;
