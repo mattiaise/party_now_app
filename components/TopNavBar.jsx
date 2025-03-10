7cd1883fe280b10dbcc5d9dabc034572d911b607
mattia.isetta <mattia.isetta@studenti.unimi.it>
2025-03-10 16:32:31 +0100

    minor chages

--- components/TopNavBar.jsx
+++ components/TopNavBar.jsx
@@ -2,24 +2,31 @@ import { Dices, MapPin, MessagesSquare } from "lucide-react";
 
 const BottomNavBar = () => {
   return (
-    //<div className="fixed top-0 left-0 w-full bg-white shadow-md border-t flex justify-around py-3">
     <div className="fixed top-0 left-0 w-full bg-white shadow-md border-t flex justify-between py-3 px-5">
-      <NavItem icon={<Dices size={24}/>} label="Logo" />
+      <NavItem icon={<Dices size={34}/>} />
       <div className="flex space-x-6">
-        <NavItem icon={<MapPin size={24}/>} label="Location" />
-        <NavItem icon={<MessagesSquare size={24} />} label="Chat" />
+        <NavButton icon={<MapPin size={24}/>} label="Location" />
+        <NavButton icon={<MessagesSquare size={24} />} label="Chat" />
       </div>
       
     </div>
   );
 };
 
-const NavItem = ({ icon, label }) => {
+const NavButton = ({ icon, label }) => {
   return (
     <button className="flex flex-col items-center text-gray-600 hover:text-blue-500">
       {icon}
       <span className="text-xs mt-1">{label}</span>
     </button>
+  );
+};
+
+const NavItem = ({ icon }) => {
+  return (
+    <div className="flex flex-col items-center text-gray-600">
+      {icon}
+    </div>
   );
 };
 
