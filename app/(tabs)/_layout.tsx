01b47f9783d793033aef0691033fe577a4c162ce
Andrea0304 <34301915+Andrea0304@users.noreply.github.com>
2025-03-11 18:21:08 +0100

    Fixed files

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from "expo-router";
+
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="home" options={{ title: "Home" }} />
+      <Tabs.Screen name="create" options={{title: "Crea", tabBarStyle: {display: "none"}, headerShown: false}} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
+}
