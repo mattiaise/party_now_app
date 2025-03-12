381bc439a3be32963cc99d5c68224664f70bb12e
mattia.isetta <mattia.isetta@studenti.unimi.it>
2025-03-12 13:52:06 +0100

    add go back button in create header

--- app/(tabs)/create/_layout.tsx
+++ app/(tabs)/create/_layout.tsx
@@ -1,10 +1,22 @@
-import { Stack } from "expo-router";
-
+import { Stack, useNavigation } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { ArrowLeft } from "lucide-react";
 
 export default function CreateLayout() {
+  const navigation = useNavigation();
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Create" }} />
+      <Stack.Screen name="index"
+        options={{
+          title: "Crea",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 10, marginRight: 5 }}>
+              <ArrowLeft size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 }
