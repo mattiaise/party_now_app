e86413b0c1d6443a5367683d148ff0a6b0b78b26
Andrea0304 <34301915+Andrea0304@users.noreply.github.com>
2025-03-27 12:24:05 +0100

    Added navigation basic/advansed create

--- app/(tabs)/(create)/index.tsx
+++ app/(tabs)/(create)/index.tsx
@@ -1,4 +1,40 @@
+import { View, Text, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+import React from 'react';
+
+const BasicRoute = () => (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", marginTop: 50 }}>
+      <Text>Basic Create</Text>
+    </View>
+);
+  
+const AdvancedRoute = () => (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", marginTop: 50 }}>
+        <Text>Advanced create</Text>
+    </View>
+);
+
+const renderScene = SceneMap({
+    first: BasicRoute,
+    second: AdvancedRoute
+});
+
+const routes = [
+    { key: 'first', title: 'Basic create' },
+    { key: 'second', title: 'Advanced create' }
+];
 
 export default function Create () {
-      
+    const layout = useWindowDimensions();
+    const [index, setIndex] = React.useState(0);
+
+    return (
+        <View> 
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: layout.width }}/>
+        </View>
+    );
 };
